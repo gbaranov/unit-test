11b6daf7c9b38b0784ca3740f3abb7ee37868f80
Georgy Baranov <floridavbv@gmail.com>
2021-02-27 14:18:25 -0700

    Test object added

--- test/test.js
+++ test/test.js
@@ -115,6 +115,12 @@ const testObject = {
     age: 20
 }
 
+const testObject2 = {
+    firstName: 'John',
+    lastName: '',
+    age: 20
+}
+
 describe('Object check', function () {
     it('Should be an object with property name - age', function () {
         expect(testObject).which.is.an('object').have.a.property('age');
